8bb012527477a7a3231b5b5b785693b27498fc19
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-01-28 18:24:30 +0000

    Initial Commit

---
+++ advice_slip/advice_slipApp.swift
@@ -0,0 +1,17 @@
+//
+//  advice_slipApp.swift
+//  advice_slip
+//
+//  Created by Zsolt Kébel on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct advice_slipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
